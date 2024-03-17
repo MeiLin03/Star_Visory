87c011c1382641434088f3beb6fe4026c0785615
MeiLin03 <155990545+MeiLin03@users.noreply.github.com>
2024-03-16 23:27:31 -0600

    Add files via upload

---
+++ MVC/modelo/conexion.php
@@ -0,0 +1,11 @@
+<?php
+
+$conexion = mysqli_connect("fdb1032.awardspace.net","4141714_sv", "Starvisory123", "4141714_sv");
+
+/*if($conexion){
+	echo "Conectado exitosamente";
+}else{
+	echo "No se ha podido conectar";
+}*/
+
+?>
